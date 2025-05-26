4fdfd83d620c98bb3cd2983f757a47def5bbf560
ShahjahanAdil <shahjahanadil01.com>
2025-05-25 23:48:28 -0700

    Contact us page remaining

--- client/src/pages/Frontend/ContactPage/Index.jsx
+++ client/src/pages/Frontend/ContactPage/Index.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ContactUs from "../../../Components/ContactUS/Index";
-import Navbar from "../../../Components/Navbar";
-import Footer from "../../../Components/Footer";
+
 export default function ContactPage() {
   return (
     <>
-      {/* <Navbar /> */}
       <ContactUs />
-      {/* <Footer /> */}
     </>
   );
 }
